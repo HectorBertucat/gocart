41f9ab3266983801dbb1c49a311dcaf85f963ee9
Hector Bertucat <hbertucat@maqprint.fr>
2023-02-03 10:05:00 +0100

    renamed <title>

--- vue/vue_template.php
+++ vue/vue_template.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <title>Verci | Hermes</title>
+    <title>Gocart</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
     <link rel="icon" href="image/icon.png" />
