38acc25c7f1f50e8cf21e973dc73951c4d1f1bea
hectorbertucat <hectorbertucat@gmail.com>
2023-03-17 22:55:37 +0100

    refactor to make file easier to understand added lastData to update only if data has changed auto refresh of all charts and date each 10 seconds

--- js/admin_dashboard.js
+++ js/admin_dashboard.js
@@ -38,10 +38,11 @@ var sellsDayOptions = {
       const index = elements[0].index;
       // day = the monday of the selected date on the datepicker + the index of the selected bar
       day = new Date($("#datepicker").val());
-      day.setDate(day.getDate() - day.getDay() + index + 1);
+      day.setDate(day.getDate() - ((day.getDay() + 6) % 7) + index);
       // format date to yyyy-mm-dd
       day = day.getFullYear() + '-' + (day.getMonth() + 1 < 10 ? '0' + (day.getMonth() + 1) : day.getMonth() + 1) + '-' + (day.getDate() < 10 ? '0' + day.getDate() : day.getDate());
       getItemsSoldDayAmount(day);
+      getItemsSoldDayQuantity(day);
       $("#datepicker").val(day);
 
       const selectedElement = elements[0];
@@ -59,8 +60,7 @@ function updateChart(chart, data) {
   chart.update();
 }
 
-// ------------ creation of charts ------------
-
+// ------------ creation of charts instances ------------
 sells_day_amount= new Chart('sells_day_amount', {
   type: 'bar',
   options: sellsDayOptions,
@@ -85,14 +85,30 @@ sells_hour_quantity = new Chart('sells_hour_quantity', {
   data: null
 });
 
-dataHoursAmount = Array(21).fill(0);
-dataDaysAmount = Array(7).fill(0);
+// ------------ data for charts ------------
+var dataHoursAmount = Array(21).fill(0);
+var dataDaysAmount = Array(7).fill(0);
+var dataHoursQuantity = Array(21).fill(0);
+var dataDaysQuantity = Array(7).fill(0);
+var nbCarts = 0;
+var turnover = 0;
 
-dataHoursQuantity = Array(21).fill(0);
-dataDaysQuantity = Array(7).fill(0);
+// ------------ data for auto update ------------
+var lastDataHoursAmount = null;
+var lastDataDaysAmount = null;
+var lastDataHoursQuantity = null;
+var lastDataDaysQuantity = null;
+var lastNbCarts = null;
+var lastTurnover = null;
 
+var emptyHours = Array(21).fill(0);
+var emptyDays = Array(7).fill(0);
+
+// ------------ data for labels ------------
 hoursLabels = ["8H", "9H", "10H", "11H", "12H", "13H", "13H", "14H", "15H", "16H", "17H", "18H", "19H", "20H", "21H"];
 daysLabels = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+
+// ------------ ajax requests ------------
 
 // update amount sold per hour of selected day
 function getItemsSoldDayAmount(day, article_type = 0, cart_id = 0) {
@@ -103,26 +119,32 @@ function getItemsSoldDayAmount(day, article_type = 0, cart_id = 0) {
         async: false,
         dataType: "json",
         success: function(data) {
-          dataHoursAmount = [];
+            // reset dataHoursAmount
+            dataHoursAmount = emptyHours.slice();
+
             data.forEach(element => {
-              dataHoursAmount[element.hour-8] = element.amount;
+                dataHoursAmount[element.hour - 8] = element.amount;
             });
+
             var dataChartHours = {
                 labels: hoursLabels,
                 datasets: [{
-                  label: "Ventes du jour",
-                  backgroundColor: "rgba(34,139,34,0.2)",
-                  borderColor: "rgba(0,100,0)",
-                  borderWidth: 2,
-                  hoverBackgroundColor: "rgba(34,139,34,0.2)",
-                  hoverBorderColor: "rgba(0,100,0)",
-                  data: dataHoursAmount,
+                    label: "Ventes du jour",
+                    backgroundColor: "rgba(34,139,34,0.2)",
+                    borderColor: "rgba(0,100,0)",
+                    borderWidth: 2,
+                    hoverBackgroundColor: "rgba(34,139,34,0.2)",
+                    hoverBorderColor: "rgba(0,100,0)",
+                    data: dataHoursAmount,
                 }]
-              };
+            };
 
-              updateChart(sells_hour_amount, dataChartHours);
+            // only update chart if data has changed
+            if (JSON.stringify(dataHoursAmount) !== JSON.stringify(lastDataHoursAmount)) {
+                updateChart(sells_hour_amount, dataChartHours);
+                lastDataHoursAmount = dataHoursAmount.slice(); // make a copy of the array
+            }
         }
-        
     });
 }
 
@@ -135,7 +157,9 @@ function getItemsSoldWeekAmount(day, article_type = 0, cart_id = 0) {
         async: false,
         dataType: "json",
         success: function(data) {
-            dataDaysAmount = [];
+            // reset dataDaysAmount
+            dataDaysAmount = emptyDays.slice();
+
             data.forEach(element => {
               dataDaysAmount[element.day] = element.amount;
             });
@@ -152,8 +176,16 @@ function getItemsSoldWeekAmount(day, article_type = 0, cart_id = 0) {
                 }]
             };
 
-            updateChart(sells_day_amount, dataChartDays);
-            
+            // if dataDaysAmount is empty, copy emptyDays to dataDaysAmount
+            if (dataDaysAmount.every((val, i, arr) => val === arr[0])) {
+                dataDaysAmount = emptyDays.slice(); // make a copy of the array
+            }
+
+            // only update chart if data has changed
+            if (JSON.stringify(dataDaysAmount) !== JSON.stringify(lastDataDaysAmount)) {
+                updateChart(sells_day_amount, dataChartDays);
+                lastDataDaysAmount = dataDaysAmount.slice(); // make a copy of the array
+            }
         }
 
     });
@@ -168,7 +200,9 @@ function getItemsSoldDayQuantity(day, article_type = 0, cart_id = 0) {
       async: false,
       dataType: "json",
       success: function(data) {
-        dataHoursQuantity = [];
+          // reset dataHoursQuantity
+          dataHoursQuantity = emptyHours.slice();
+
           data.forEach(element => {
             dataHoursQuantity[element.hour-8] = element.quantity;
           });
@@ -185,7 +219,16 @@ function getItemsSoldDayQuantity(day, article_type = 0, cart_id = 0) {
               }]
             };
 
-            updateChart(sells_hour_quantity, dataChartHours);
+          // if dataHoursQuantity is empty, copy emptyHours to dataHoursQuantity
+          if (dataHoursQuantity.every((val, i, arr) => val === arr[0])) {
+              dataHoursQuantity = emptyHours.slice(); // make a copy of the array
+          }
+
+          // only update chart if data has changed
+          if (JSON.stringify(dataHoursQuantity) !== JSON.stringify(lastDataHoursQuantity)) {
+              updateChart(sells_hour_quantity, dataChartHours);
+              lastDataHoursQuantity = dataHoursQuantity.slice(); // make a copy of the array
+          }
       }
       
   });
@@ -200,7 +243,9 @@ function getItemsSoldWeekQuantity(day, article_type = 0, cart_id = 0) {
       async: false,
       dataType: "json",
       success: function(data) {
-          dataDaysQuantity = [];
+          // reset dataDaysQuantity
+          dataDaysQuantity = emptyDays.slice();
+
           data.forEach(element => {
             dataDaysQuantity[element.day] = element.quantity;
           });
@@ -217,30 +262,50 @@ function getItemsSoldWeekQuantity(day, article_type = 0, cart_id = 0) {
               }]
           };
 
-          updateChart(sells_day_quantity, dataChartDays);
-          
-      }
+          // if dataDaysQuantity is empty, copy emptyDays to dataDaysQuantity
+          if (dataDaysQuantity.every((val, i, arr) => val === arr[0])) {
+              dataDaysQuantity = emptyDays.slice(); // make a copy of the array
+          }
 
+          // only update chart if data has changed
+          if (JSON.stringify(dataDaysQuantity) !== JSON.stringify(lastDataDaysQuantity)) {
+              updateChart(sells_day_quantity, dataChartDays);
+              lastDataDaysQuantity = dataDaysQuantity.slice(); // make a copy of the array
+          }
+      }
   });
 }
 
-function getTurnover(start, end) {
-  var url = "index.php?controller=admin_dashboard&chart=turnover&start=" + start + "&end=" + end;
-  $.ajax({
-      url: url,
-      type: "GET",
-      async: false,
-      dataType: "json",
-      success: function(data) {
-        // round turnover to 2 decimals
-        turnover = Math.round(data[0][0] * 100) / 100;
+// get total turnover of selected period
+function getTurnover() {
+    let start = $("#datepickerTurnoverStart").val();
+    let end = $("#datepickerTurnoverEnd").val();
 
-        // change element with turnover id to turnover
-        $("#turnover").html(turnover + "€");
-      }});
+    var url = "index.php?controller=admin_dashboard&chart=turnover&start=" + start + "&end=" + end;
+    $.ajax({
+        url: url,
+        type: "GET",
+        async: false,
+        dataType: "json",
+        success: function(data) {
+            // round turnover to 2 decimals
+            if (data[0][0] != null) {
+                turnover = Math.round(data[0][0] * 100) / 100;
+            } else {
+                turnover = 0.00;
+            }
+
+            if (turnover != lastTurnover) {
+                lastTurnover = turnover;
+                $("#turnover").html(turnover + " €");
+            }
+        }});
 }
 
-function getNbCartsWithStatus(status) {
+// get total number of carts of selected status
+function getNbCartsWithStatus() {
+    let status = $("#cartStatus").val();
+
     var url = "index.php?controller=admin_dashboard&chart=nb_carts_with_status&status_id=" + status;
     $.ajax({
         url: url,
@@ -248,70 +313,80 @@ function getNbCartsWithStatus(status) {
         async: false,
         dataType: "json",
         success: function(data) {
-            // change element with status id to status
-            $("#nbCarts").html(data[0][0]);
+            nbCarts = data[0][0];
+
+            if (nbCarts != lastNbCarts) {
+                lastNbCarts = nbCarts;
+                $("#nbCarts").html(nbCarts);
+            }
         }});
 }
 
-function updateItemsSold(date, article_type = 0, cart_id = 0) {
+// update all charts
+function updateItemsSold() {
+    let date = $("#datepicker").val();
+    let article_type = $("#article_type").val();
+    let cart_id = $("#carts").val();
     getItemsSoldDayAmount(date, article_type, cart_id);
     getItemsSoldWeekAmount(date, article_type, cart_id);
     getItemsSoldDayQuantity(date, article_type, cart_id);
     getItemsSoldWeekQuantity(date, article_type, cart_id);
 }
 
-// on element with id datepicker change, update charts
+// update all charts, turnover and number of carts with status
+function updateAll() {
+    updateItemsSold();
+    getTurnover();
+    getNbCartsWithStatus();
+}
+
+// ------------ JQuery on elements change ------------
 $("#datepicker").change(function() {
-    getItemsSoldDayAmount($(this).val());
-    getItemsSoldWeekAmount($(this).val());
-    getItemsSoldDayQuantity($(this).val());
-    getItemsSoldWeekQuantity($(this).val());
+    updateItemsSold();
 });
 
 $("#datepickerTurnoverStart").change(function() {
-    getTurnover($(this).val(), $("#datepickerTurnoverEnd").val());
+    getTurnover();
 });
 
 $("#datepickerTurnoverEnd").change(function() {
-    getTurnover($("#datepickerTurnoverStart").val(), $(this).val());
+    getTurnover();
 });
 
 // on select change, update charts
 $("#article_type").change(function() {
-  article_type = $(this).val();
-  cart_id = $("#carts").val();
-
-  updateItemsSold($("#datepicker").val(), article_type, cart_id);
+  updateItemsSold();
 });
 
 // on select change, update charts
 $("#carts").change(function() {
-  let article_type = $("#article_type").val();
-  let cart_id = $(this).val();
-
-  updateItemsSold($("#datepicker").val(), article_type, cart_id);
+  updateItemsSold();
 });
 
 // on status carts change, update number of carts with status
 $("#cartStatus").change(function() {
-    let cart_status = $(this).val();
-    getNbCartsWithStatus(cart_status);
+    getNbCartsWithStatus();
 });
 
 $(document).ready(function () {
-  // get current date
-  const today = new Date();
-  // format date to yyyy-mm-dd
-  const date = today.getFullYear() + '-' + (today.getMonth() + 1 < 10 ? '0' + (today.getMonth() + 1) : today.getMonth() + 1) + '-' + (today.getDate() < 10 ? '0' + today.getDate() : today.getDate());
-  let cart_status = $("#cartStatus").val();
+    // get current date
+    const today = new Date();
+    // format date to yyyy-mm-dd
+    const date = today.getFullYear() + '-' + (today.getMonth() + 1 < 10 ? '0' + (today.getMonth() + 1) : today.getMonth() + 1) + '-' + (today.getDate() < 10 ? '0' + today.getDate() : today.getDate());
 
-  // set input "#datepicker" to today's date
-  $("#datepicker").val(date);
-  $("#datepickerTurnoverStart").val(date);
-  $("#datepickerTurnoverEnd").val(date);
+    // set input "#datepicker" to today's date
+    $("#datepicker").val(date);
+    $("#datepickerTurnoverStart").val(date);
+    $("#datepickerTurnoverEnd").val(date);
 
-  updateItemsSold(date);
-  getTurnover(date, date);
-  getNbCartsWithStatus(cart_status);
+    // set cart status to 3 (current)
+    $("#cartStatus").val(3);
 
+    // update all charts
+    updateAll();
 });
+
+var autoRefresh = window.setInterval(function(){
+    // update all charts every 10 seconds
+    updateAll();
+}, 10000);
